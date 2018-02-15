d96b91001d8f2c684d4b29898ab37fbec9c0647e
Werner Brynard <werner.brynard@gmail.com>
2018-02-15 14:42:04 +0200

    Updated threshold

--- MailCheck/MailCheck.cs
+++ MailCheck/MailCheck.cs
@@ -14,7 +14,7 @@ namespace MailCheck
 
         public string[] DefaultDomains = new string[] { "msn.com", "bellsouth.net", "telus.net", "comcast.net", "optusnet.com.au", "earthlink.net", "qq.com", "sky.com", "icloud.com", "mac.com", "sympatico.ca", "googlemail.com", "att.net", "xtra.co.nz", "web.de", "cox.net", "gmail.com", "ymail.com", "aim.com", "rogers.com", "verizon.net", "rocketmail.com", "google.com", "optonline.net", "sbcglobal.net", "aol.com", "me.com", "btinternet.com", "charter.net", "shaw.ca" };
 
-        public string[] DefaultSecondLevelDomains = new string[] { "yahoo", "hotmail", "live", "outlook", "gmx" };
+        public string[] DefaultSecondLevelDomains = new string[] { "yahoo", "hotmail", "live", "outlook" };
 
         public string[] DefaultTopLevelDomains = new string[] { "com", "com.au", "com.tw", "ca", "co.za", "co.nz", "co.uk", "de", "fr", "it", "ru", "net", "org", "edu", "gov", "jp", "nl", "kr", "se", "eu", "ie", "co.il", "us", "at", "be", "dk", "hk", "es", "gr", "ch", "no", "cz", "in", "net", "net.au", "info", "biz", "mil", "co.jp", "sg", "hu", "uk" };
 
